33db1d7a4f3e488e9235114fc400745ce2e19258
panhayu <panha.yuu@gmail.com>
2026-04-27 16:15:32 +0700

    feat: initialize new Nuxt 4 project structure

---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+const { t, locale } = useI18n()
+const { locales } = useI18n()
+
+</script>
+
+<template>
+    <div>
+        <form>
+            <label for="locale-select">{{ $t('language') }}: </label>
+            <select id="locale-select" v-model="locale">
+                <option v-for="locale in locales" :key="locale.code" :value="locale.code">
+                    {{ locale.name }}
+                </option>
+            </select>
+        </form>
+        <h1 class="text-3xl text-blue-500">{{ t('welcome') }}</h1>
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: ['@nuxtjs/i18n', '@nuxt/fonts', '@nuxtjs/tailwindcss'],
+
+  css: ['~/assets/css/main.css'],
+
+  fonts: {
+    families: [
+      { name: 'Suwannaphum', provider: 'google' },
+    ],
+  },
+
+  i18n: {
+    defaultLocale: 'en',
+    strategy: 'prefix_except_default',
+    locales: [
+      { code: 'en', language: 'en-US', name: 'English', file: 'en.json' },
+      { code: 'km', language: 'km-KH', name: 'ខ្មែរ', file: 'km.json' }
+    ],
+  }
+})
